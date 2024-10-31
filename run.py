ed7106018415d1275e5795d8f320beb70f360480
neliz3 <eliza.nadrega14@gmail.com>
2024-10-31 09:19:16 +0200

    feat: add flake8

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask
-
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
@@ -9,4 +9,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555, debug=True)
-
